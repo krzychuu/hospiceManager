44aa7a25a5dd593ab71f864dcf43cdf74d94fb29
krzychuu <krzych.stasiak@gmail.com>
2014-12-30 17:13:05 +0100

    Initial version for event view
    Initial version for event view

---
+++ src/Hospice/SiteBundle/Form/EventType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Hospice\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('start')
+            ->add('stop')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Hospice\SiteBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hospice_sitebundle_event';
+    }
+}
